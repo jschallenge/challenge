ccf10324e4d3034d112625ee470250e366ec2db2
Unknown <jschallenge@gmail.com>
2017-06-06 16:12:07 +0200

    Test refactor

--- test/test.js
+++ test/test.js
@@ -1,26 +1,15 @@
 var assert = require('assert');
 var calculateMidprice = require('../site/midprice.js')
-
-var midprice = {};
-var testMessage = {
-  "name": "usdjpy",
-  "bestBid": 106.7297012204255,
-  "bestAsk": 107.25199883791178,
-  "openBid": 107.22827132623534,
-  "openAsk": 109.78172867376465,
-  "lastChangeAsk": -4.862314256927661,
-  "lastChangeBid": -2.8769211401569663
-}
-var midPrice=106.99085002916864
+var mockData = require('../test/mockData.js')
 
 
 describe('Challenge', function() {
   describe('#calculateMidprice()', function() {
     it('should calculate the mid price', function() {
-        
-        calculateMidprice(midprice, testMessage)
-        var elemMidprice = midprice[testMessage["name"]][0][1]        
-         assert.equal(midPrice, elemMidprice);
+        var midprice = {};
+        calculateMidprice(midprice, mockData.testMessage)
+        var elemMidprice = midprice[mockData.testMessage["name"]][0][1]        
+        assert.equal(mockData.expectedMidPrice, elemMidprice);
         
     });
   });
